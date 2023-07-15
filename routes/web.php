683f24e8b38887c9406d40e512dfe7e8ceb24b9c
mxculexerRyan <mxculexer@gmail.com>
2023-07-15 13:53:25 +0300

    updated routes

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-use  App\Http\Controllers\HomeController;
+use  App\Http\Controllers\DashboardController;
 
 /*
 |--------------------------------------------------------------------------
@@ -20,9 +20,5 @@ Route::get('/', function () {
 
 Route::middleware(['auth:sanctum',config('jetstream.auth_session'),
     'verified'])->group(function () {
-        Route::get('/dashboard', function () {
-            return view('dashboard');
-        })->name('dashboard');
+        Route::get('/dashboard', [DashboardController::class, "index"])->name('dashboard');
 });
-
-Route::get('redirects', [HomeController::class, "index"]);
