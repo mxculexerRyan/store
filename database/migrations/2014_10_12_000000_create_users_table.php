1d4e137d1a07d33bec1b72c2de50dbb532248bf7
mxculexerRyan <mxculexer@gmail.com>
2023-07-15 14:18:21 +0300

    added the assets folder into public

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,6 +15,7 @@ return new class extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->string('phone')->nullable();
             $table->timestamp('email_verified_at')->nullable();
             $table->unsignedBigInteger('role_id')->default(1);
             $table->string('password');
---
+++ database/seeders/UserSeeder.php
@@ -0,0 +1,44 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+
+class UserSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('users')->insert([
+            [
+                'name' => 'user',
+                'email' => 'user@store.com',
+                'role_id' => '1',
+                'password' => Hash::make('user@123'),
+                'created_at' => date("Y-m-d H:i:s"),
+                'updated_at' => date("Y-m-d H:i:s"),
+            ],
+            [
+                'name' => 'owner',
+                'email' => 'owner@store.com',
+                'role_id' => '2',
+                'password' => Hash::make('owner@123'),
+                'created_at' => date("Y-m-d H:i:s"),
+                'updated_at' => date("Y-m-d H:i:s"),
+            ],
+            [
+                'name' => 'admin',
+                'email' => 'admin@store.com',
+                'role_id' => '3',
+                'password' => Hash::make('admin@123'),
+                'created_at' => date("Y-m-d H:i:s"),
+                'updated_at' => date("Y-m-d H:i:s"),
+            ],
+        ]);
+    }
+}
