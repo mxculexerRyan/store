e7705e348d9861f580858decec6ff678aaea4706
mxculexerRyan <mxculexer@gmail.com>
2023-07-15 13:51:42 +0300

    controller to check role

---
+++ app/Http/Controllers/DashboardController.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class DashboardController extends Controller
+{
+    public function index(){
+        if(isset(Auth::user()->role_id)){
+            $role = Auth::user()->role_id;
+            if($role == '2'){
+                return view('owner.dashboard');
+            }
+            if($role == '3'){
+
+                return view('admin.dashboard');
+            }else{
+                return view('dashboard');
+            }
+
+        }else{
+            return redirect()->route('login');
+        }
+    }
+}
