f59fbfbbc0c82370220075b49eb49a765f4b6cb1
mxculexerRyan <mxculexer@gmail.com>
2023-07-16 11:49:48 +0300

    added the username & phone columns

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -14,8 +14,9 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->string('username')->unique();
             $table->string('email')->unique();
-            $table->string('phone')->nullable();
+            $table->string('phone')->unique();
             $table->timestamp('email_verified_at')->nullable();
             $table->unsignedBigInteger('role_id')->default(1);
             $table->string('password');
