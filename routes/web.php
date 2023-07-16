36000f7c0e30c1cbca1ae9cf41e97871219312f4
mxculexerRyan <mxculexer@gmail.com>
2023-07-16 13:35:45 +0300

    new logout route under the no-cache midleware

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,12 @@ use  App\Http\Controllers\DashboardController;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
+Route::get('/', function () { return view('welcome'); });
 Route::middleware(['auth:sanctum',config('jetstream.auth_session'),
     'verified'])->group(function () {
         Route::get('/dashboard', [DashboardController::class, "index"])->name('dashboard');
+});
+
+Route::group(['middleware' => 'prevent-back-history'],function(){
+    Route::get('/user/logout', [DashboardController::class, "Logout"])->name('user.logout');
 });
