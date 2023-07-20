16d908ab42ae49c81041691934e4e7c568e4d18a
mxculexerRyan <mxculexer@gmail.com>
2023-07-20 21:40:32 +0300

    adding features in controller

--- app/Http/Controllers/FeaturesController.php
+++ app/Http/Controllers/FeaturesController.php
@@ -18,7 +18,23 @@ class FeaturesController extends Controller
         return view('features.profile', compact('profileData', 'role'));
     }
 
-    public function profile_update(){
-        return view('dashboard');
+    public function profile_update(Request $request){
+
+        $id = Auth::user()->id;
+        $data = user::find($id);
+        $data->name = $request->name;
+        $data->uname = $request->uname;
+        $data->email = $request->email;
+        $data->phone = $request->phone;
+
+        if($request->file('profile_photo_path')){
+            $file = $request->file('profile_photo_path');
+            $filename = "IMG_".date('YmdHi')."_".$file->getClientOriginalName();
+            $file->move(public_path('uploads/images'),$filename);
+            $data['profile_photo_path'] = $filename;
+        }
+
+        $data->save();
+        return redirect()->back();
     }
 }
