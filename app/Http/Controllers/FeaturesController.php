58b8df975db939e2932afbab1c9572e8925c3f09
mxculexerRyan <mxculexer@gmail.com>
2023-07-20 07:23:58 +0300

    fetching data from user and roles

--- app/Http/Controllers/FeaturesController.php
+++ app/Http/Controllers/FeaturesController.php
@@ -5,13 +5,16 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use App\Models\User;
+use App\Models\Role;
 
 class FeaturesController extends Controller
 {
     public function index(){
         $id = Auth::user()->id;
         $profileData = user::find($id);
+        $role_id = $profileData->role_id;
+        $role = role::find($role_id);
         
-        return view('features.profile', compact('profileData'));
+        return view('features.profile', compact('profileData', 'role'));
     }
 }
