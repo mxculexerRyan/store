23e88aee0b23aab4f7738d007e3e313dd1bc3d46
mxculexerRyan <mxculexer@gmail.com>
2023-07-20 21:41:19 +0300

    toaster file js

--- resources/views/components/tempscripts.blade.php
+++ resources/views/components/tempscripts.blade.php
@@ -15,3 +15,28 @@
 	<!-- Custom js for this page -->
     <script src="{{ asset('backend/assets/js/dashboard-dark.js') }}"></script>
 	<!-- End custom js for this page -->
+
+	<script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.js"></script>
+
+	<script>
+	 @if(Session::has('message'))
+	 var type = "{{ Session::get('alert-type','info') }}"
+	 switch(type){
+		case 'info':
+		toastr.info(" {{ Session::get('message') }} ");
+		break;
+	
+		case 'success':
+		toastr.success(" {{ Session::get('message') }} ");
+		break;
+	
+		case 'warning':
+		toastr.warning(" {{ Session::get('message') }} ");
+		break;
+	
+		case 'error':
+		toastr.error(" {{ Session::get('message') }} ");
+		break; 
+	 }
+	 @endif 
+	</script>
