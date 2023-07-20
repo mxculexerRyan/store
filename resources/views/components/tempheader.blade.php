8f4128b239a3bebf7d60b3ce9a92093309635de5
mxculexerRyan <mxculexer@gmail.com>
2023-07-20 21:41:10 +0300

    toaster file css

--- resources/views/components/tempheader.blade.php
+++ resources/views/components/tempheader.blade.php
@@ -40,6 +40,8 @@
     <link rel="shortcut icon" href="{{ asset('/images/favicon_blue.svg') }}" />
 	{{-- @vite('resources/css/app.css') --}}
 
+	<link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.css" >
+
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.4/jquery.min.js"></script>
 </head>
 <body>
