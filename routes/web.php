7ce52d67cdf1ed2e7c118e2b3978b394c77555fe
mxculexerRyan <mxculexer@gmail.com>
2023-07-22 00:30:20 +0300

    route to corespond with account.update

--- routes/web.php
+++ routes/web.php
@@ -66,6 +66,7 @@ Route::middleware(['auth', 'role:3'])->group(function (){
     Route::get('/features/profile', [FeaturesController::class, "index"])->name('features.profile');
     Route::post('/features/profile/update', [FeaturesController::class, "profile_update"])->name('features.profile.update');
     Route::get('/features/account', [FeaturesController::class, "account"])->name('features.account');
+    Route::post('/features/account/update', [FeaturesController::class, "account_update"])->name('features.account.update');
 });
     Route::group(['middleware' => 'prevent-back-history'],function(){
     Route::get('/user/logout', [DashboardController::class, "Logout"])->name('user.logout');
