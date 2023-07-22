e2df3d4f0b925a58c72c914d562b7f264ace5c02
mxculexerRyan <mxculexer@gmail.com>
2023-07-22 23:54:00 +0300

    tags blade

--- resources/views/activities/products/tags.blade.php
+++ resources/views/activities/products/tags.blade.php
@@ -1,7 +1,68 @@
 <x-pagetop/>
-    <div class="page-content">
-        
+<div class="page-content">
+
+    <nav class="page-breadcrumb">
+        <ol class="breadcrumb">
+            <li class="breadcrumb-item"><a href="#">Tables</a></li>
+            <li class="breadcrumb-item" aria-current="page">Data Table</li>
+        </ol>
+    </nav>
+
+    <div class="row">
+        <div class="col-md-12 grid-margin stretch-card">
+            <div class="card">
+                <div class="card-body">
+                    <div class="mb-3 d-flex justify-content-between align-items-center">
+                        <div>
+                            <h6 class="card-title">Tags List</h6>
+                        </div>
+                        <div>
+                            <button type="button" class="items-center d-flex btn btn-primary btn-icon-text" data-bs-toggle="modal" data-bs-target="#addTagModal">
+                                <i class="btn-icon-prepend" data-feather="tag"></i>Add Tag 
+                            </button>
+                        </div>
+                    </div>
+                    
+                    <div class="table-responsive">
+                        <table id="dataTableExample" class="table">
+                            <thead>
+                            <tr>
+                                <th>No.</th>
+                                <th>Tag Name</th>
+                                <th>Tag Key</th>
+                                <th>Tag Description</th>
+                                <th>Tag Status</th>
+                                <th>Edit</th>
+                                <th>Delete</th>
+                            </tr>
+                            </thead>
+                            <tbody>
+                            @foreach ($tagData as $key => $item)
+                            <tr>
+                                <td>{{ $key+1 }}</td>
+                                <td>{{ $item->tagname }}</td>
+                                <td>{{ $item->tagkey }}</td>
+                                <td>{{ $item->tagdesc }}</td>
+                                <td>@if ($item->tagstatus == "available")
+                                    <span class="border badge border-success text-success">{{ $item->tagstatus }}</span>
+                                @else
+                                <span class="border badge border-warning text-warning">{{ $item->tagstatus }}</span>
+                                @endif</td>
+                                <td><button type="button" class="btn btn-inverse-warning btn-icon" data-bs-toggle="modal" data-bs-target="#editTagModal"><i data-feather="edit"></i></button></td>
+                                <td><button type="button" class="btn btn-inverse-danger btn-icon" data-bs-toggle="modal" data-bs-target="#deleteTagModal"><i data-feather="trash-2"></i></button></td>
+                            </tr>
+                            @endforeach
+                            </tbody>
+                        </table>
+                    </div>
+
+                    <x-addtagmodal/>
+                </div>
+            </div>
+        </div>
     </div>
+
+</div>
 <x-pagebottom/>
 {{-- <script src="{{ asset('/frontend/assets/js/trade/sell.js') }}"></script> --}}
 </body>
