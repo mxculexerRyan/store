f71a75e11819b600043934a6a58c958e5f1e1cb8
mxculexerRyan <mxculexer@gmail.com>
2023-07-23 00:01:34 +0300

    new brands migration

---
+++ database/migrations/2023_07_22_205633_create_brands_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('brands', function (Blueprint $table) {
+            $table->id('brand_id');
+            $table->string('brand_name');
+            $table->string('brand_key');
+            $table->string('brand_desc');
+            $table->enum('brand_status', ['available', 'unavailable'])->default('available');
+            $table->unsignedBigInteger('tag_id');
+            $table->foreign('tag_id')->references('tag_id')->on('tags')->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('brands');
+    }
+};
