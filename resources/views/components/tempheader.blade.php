c9348676e2a898deee7185ebbf33aa38cb2fe781
mxculexerRyan <mxculexer@gmail.com>
2023-07-23 10:24:06 +0300

    add sweet alert css

--- resources/views/components/tempheader.blade.php
+++ resources/views/components/tempheader.blade.php
@@ -23,6 +23,7 @@
 	<!-- Plugin css for this page -->
 	<link rel="stylesheet" href="{{ asset('backend/assets/vendors/flatpickr/flatpickr.min.css') }}">
 	<link rel="stylesheet" href="{{ asset('backend/assets/vendors/datatables.net-bs5/dataTables.bootstrap5.css') }}">
+	<link rel="stylesheet" href="{{ asset('backend/assets/vendors/sweetalert2/sweetalert2.min.css') }}">
 	<!-- End plugin css for this page -->
 
 	<!-- inject:css -->
