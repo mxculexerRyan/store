04b0b7a7539a48e24e30d4db58a9e7a8410ea186
mxculexerRyan <mxculexer@gmail.com>
2023-07-25 02:48:11 +0300

    budjet blade

---
+++ resources/views/roster/accounting/budjet.blade.php
@@ -0,0 +1,8 @@
+<x-pagetop/>
+    <div class="page-content">
+        
+    </div>
+<x-pagebottom/>
+{{-- <script src="{{ asset('/frontend/assets/js/trade/sell.js') }}"></script> --}}
+</body>
+</html> 
