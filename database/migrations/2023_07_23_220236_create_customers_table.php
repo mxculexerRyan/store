585aa450078969990fb3b06cc94d11dbdde12028
mxculexerRyan <mxculexer@gmail.com>
2023-07-24 15:30:03 +0300

    migration file of the customer table in database

---
+++ database/migrations/2023_07_23_220236_create_customers_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('customers', function (Blueprint $table) {
+            $table->id();
+            $table->string('customer_name');
+            $table->string('customer_email');
+            $table->string('customer_phone');
+            $table->string('customer_location');
+            $table->string('customer_bank');
+            $table->string('customer_account');
+            $table->string('photo', 2048)->nullable();
+            $table->enum('customer_status', ['available', 'unavailable'])->default('available');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('customers');
+    }
+};
