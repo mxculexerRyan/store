d56535ccf2419ced6404fbc15d8da4d390a51f55
mxculexerRyan <mxculexer@gmail.com>
2023-07-24 22:28:07 +0300

    budjet data seeder

---
+++ database/seeders/BudjetSeeder.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class BudjetSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('budjets')->insert([
+            [
+                'budjet_name' => 'Transportation',
+                'projected_amount' => '20000',
+                'spent_amount' => '0',
+                'created_at' => date("Y-m-d H:i:s"),
+                'updated_at' => date("Y-m-d H:i:s"),
+            ],
+        ]);
+    }
+}
