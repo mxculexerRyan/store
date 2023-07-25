90680e1147779eaa1a255e0360c982c0b74c1d00
mxculexerRyan <mxculexer@gmail.com>
2023-07-25 11:14:09 +0300

    create the purchases table migration

---
+++ database/migrations/2023_07_25_080216_create_purchases_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('purchases', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('oder_id');
+            $table->unsignedBigInteger('item_name');
+            $table->decimal('buying_price');
+            $table->decimal('purchased_quantity');
+            $table->decimal('vat_fees');
+            $table->decimal('item_discount')->default(0);
+            $table->foreign('oder_id')->references('id')->on('oders')->onUpdate('cascade');
+            $table->foreign('item_name')->references('id')->on('products')->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('purchases');
+    }
+};
