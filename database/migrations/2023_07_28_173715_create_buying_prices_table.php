7a139a82362b1ee47cc94a85189994031845c0d5
mxculexerRyan <mxculexer@gmail.com>
2023-07-29 01:01:54 +0300

    buying prices table

---
+++ database/migrations/2023_07_28_173715_create_buying_prices_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('buying_prices', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger("product_id");
+            $table->unsignedBigInteger("supplier_id");
+            $table->decimal("buying_price");
+            $table->enum("status", ["Available", "Unavailable"])->default("Available");
+            $table->foreign('product_id')->references('id')->on('products')->onUpdate('cascade');
+            $table->foreign('supplier_id')->references('id')->on('suppliers')->onUpdate('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('buying_prices');
+    }
+};
