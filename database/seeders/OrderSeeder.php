820c54c618f18779ce6b5b167bf67a1d1dd24cf1
mxculexerRyan <mxculexer@gmail.com>
2023-07-28 20:40:04 +0300

    create order table seeder

--- database/seeders/OrderSeeder.php
+++ database/seeders/OrderSeeder.php
@@ -14,19 +14,19 @@ class OrderSeeder extends Seeder
     public function run(): void
     {
         DB::table('orders')->insert([
-            // [
-            //     'items_quantity' => '10',
-            //     'order_value'    => '23800',
-            //     'paid_amount'    => '20000',
-            //     'order_discount' => '3800',
-            //     'shipping_fees'  => '200',
-            //     'vat_fees'       => '4284',
-            //     'order_type'     => 'order_out',
-            //     'from'           => '1',
-            //     'to'             => '1',
-            //     'created_at'     => date("Y-m-d H:i:s"),
-            //     'updated_at'     => date("Y-m-d H:i:s"),
-            // ],
+            [
+                'items_quantity' => '10',
+                'order_value'    => '23800',
+                'paid_amount'    => '20000',
+                'order_discount' => '3800',
+                'shipping_fees'  => '200',
+                'vat_fees'       => '4284',
+                'order_type'     => 'order_out',
+                'from'           => '1',
+                'to'             => '1',
+                'created_at'     => date("Y-m-d H:i:s"),
+                'updated_at'     => date("Y-m-d H:i:s"),
+            ],
             [
                 'items_quantity' => '10',
                 'order_value'    => '23800',
