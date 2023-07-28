ebbac862ca100b930dd6ad640134b7ca4698ffd6
mxculexerRyan <mxculexer@gmail.com>
2023-07-29 01:03:10 +0300

    selling price modal

---
+++ app/Models/Prices/Selling_price.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Models\Prices;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Selling_price extends Model
+{
+    use HasFactory;
+}
