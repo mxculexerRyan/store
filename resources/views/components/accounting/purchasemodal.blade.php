d6424584a7c519d9cba5ede048571c0bb48a51da
mxculexerRyan <mxculexer@gmail.com>
2023-07-28 20:40:24 +0300

    add purchases modal blade

---
+++ resources/views/components/accounting/purchasemodal.blade.php
@@ -0,0 +1,36 @@
+<div class="modal fade" id="editPurchasesModal" tabindex="-1" aria-labelledby="editPurchasesModalLabel" aria-hidden="true">
+    <div class="modal-dialog">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h3 class="modal-title" id="editPurchasesModalLabel">Edit Items Form</h3>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="btn-close"></button>
+            </div>
+            
+            <form class="forms-sample" method="POST" action="{{ route('purchases.edit') }}">
+                @csrf
+                <div class="modal-body">
+                    <div class="mb-3">
+                        <label for="item_name" class="form-label">Item Name</label>
+                        <input type="text" class="form-control @error('item_name') is-invalid @enderror" id="editItem_name" name="item_name" autocomplete="off" placeholder="Item Name">
+                        @error('item_name')
+                            <span class="text-danger">{{ $message }}</span>
+                        @enderror
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                    <button type="submit" class="btn btn-primary">Save changes</button>
+                </div>
+            </form>
+        </div>
+    </div>
+</div>
+
+@if (Session::has('errors'))
+    <script>
+        $(document).ready(function(){
+            console.log("object");
+            $('#addPurchasesModal').modal('show'); 
+        });
+    </script>
+@endif
