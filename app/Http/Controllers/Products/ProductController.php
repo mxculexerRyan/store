4d5cb9d523dbf46e4d1fdd6364c48448a979a3fc
mxculexerRyan <mxculexer@gmail.com>
2023-07-30 07:42:50 +0300

    updated product controller

--- app/Http/Controllers/Products/ProductController.php
+++ app/Http/Controllers/Products/ProductController.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use DB;
 use App\Models\Brand;
 use App\Models\Product;
+use App\Models\Supplier;
 class ProductController extends Controller
 {
     public function index(){ 
@@ -15,6 +16,28 @@ class ProductController extends Controller
         return view('activities.products.product', compact('productData'));
     }
 
+    public function prodlist(){
+        $id = $_GET['id'];
+        $productData = Product::select("*")->where("product_status", "available")->get();
+        return response()->json([
+            'msg' => view('activities.products.productlist', compact('productData', 'id'))->render(),
+        ]);
+    }
+
+    public function prodsupp(){
+        $id = $_GET['id'];
+        $supplierData = Supplier::select("*")->where("supplier_status", "available")->get();
+        return response()->json([
+            'msg' => view('activities.products.productsuppliers', compact('supplierData', 'id'))->render(),
+        ]);
+    }
+
+    public function prodprices(){
+        $id = $_GET['id'];
+        $supplierData = DB::table('buying_prices')->join('suppliers', 'suppliers.id', '=', 'buying_prices.supplier_id' )
+        ->join('products', 'products.id', '=', 'buying_prices.product_id')->where("buying_prices.product_id", $id)->get();
+        return response()->json(array('msg'=> $supplierData), 200);
+    }
     public function add(Request $request){
 
         $request->validate([
