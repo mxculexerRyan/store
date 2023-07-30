f05e966ea09a1fc08310d5b1c66f35a7b46e6763
mxculexerRyan <mxculexer@gmail.com>
2023-07-30 09:09:25 +0300

    new asset model

---
+++ app/View/Components/accounting/assets.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\View\Components\accounting;
+
+use Closure;
+use Illuminate\Contracts\View\View;
+use Illuminate\View\Component;
+
+class assets extends Component
+{
+    /**
+     * Create a new component instance.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     */
+    public function render(): View|Closure|string
+    {
+        return view('components.accounting.assets');
+    }
+}
