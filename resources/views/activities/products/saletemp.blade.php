694931f20ac895fcda64df164b403d80e8fa2511
mxculexerRyan <mxculexer@gmail.com>
2023-07-30 15:53:17 +0300

    new blade sales temp

---
+++ resources/views/activities/products/saletemp.blade.php
@@ -0,0 +1,18 @@
+<tr>
+    <td>{{ $id }}</td>
+    <td>
+        <select class="form-select" id="prod{{ $id }}" name="product_name[]" onchange="getsprice(this);">
+            <option value="" selected disabled>Select Product</option>
+            @foreach ($productData as $key => $item)
+                <option value="{{ $item->id }}">{{ $item->product_key }} - {{ $item->product_name }}</option>
+            @endforeach
+        </select>
+        @error('product_name')
+            <span class="text-danger">{{ $message }}</span>
+        @enderror
+    </td>
+    <td><input type="text" class="form-control" id="price{{ $id }}" name="name"></td>
+    <td><input type="text" class="form-control" id="sprice{{ $id }}" name="name"></td>
+    <td><input type="text" class="form-control" id="quantity{{ $id }}" name="name" disabled onkeyup="getTotal(this)"></td>
+    <td><input type="text" class="form-control" id="total{{ $id }}" name="name" disabled value="0"></td>
+</tr>
