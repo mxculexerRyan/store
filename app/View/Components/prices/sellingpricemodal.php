e56b93e3fe93f922a963ba1bf1c95c0a2e17f7a2
mxculexerRyan <mxculexer@gmail.com>
2023-07-31 19:57:19 +0300

    new component selling price modal

--- app/View/Components/prices/sellingpricemodal.php
+++ app/View/Components/prices/sellingpricemodal.php
@@ -25,7 +25,7 @@ class sellingpricemodal extends Component
     public function render(): View|Closure|string
     {
         // $productId = DB::table('products')->distinct()->get();
-        $productId = Buying_prices::distinct()->get();
+        $productId = Buying_prices::select('product_id')->distinct()->get();
 
         return view('components.prices.sellingpricemodal', compact("productId"));
     }
