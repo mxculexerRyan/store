f621a946e43d12d103c7f7e6e46aff9abfee289f
mxculexerRyan <mxculexer@gmail.com>
2023-07-31 19:56:49 +0300

    new seeder customer seeder

---
+++ database/seeders/CustomerSeeder.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use DB;
+
+class CustomerSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        DB::table('customers')->insert([
+            [
+                'customer_name' => 'Maulid Haruna',
+                'customer_email' => 'maulid@store.com',
+                'customer_phone' => '(+255) 678-096-234',
+                'customer_location' => 'Musoma - Mara',
+                'customer_bank' => 'NBC',
+                'customer_account' => '4567-9545-9856-9876',
+                'created_at' => date("Y-m-d H:i:s"),
+                'updated_at' => date("Y-m-d H:i:s"),
+            ],
+            [
+                'customer_name' => 'Makaranga Joseph',
+                'customer_email' => 'makaranga@store.com',
+                'customer_phone' => '(+255) 618-096-234',
+                'customer_location' => 'Musoma - Mara',
+                'customer_bank' => 'NBC',
+                'customer_account' => '4567-9525-9856-9876',
+                'created_at' => date("Y-m-d H:i:s"),
+                'updated_at' => date("Y-m-d H:i:s"),
+            ],
+            [
+                'customer_name' => 'Masanyiwa Juma',
+                'customer_email' => 'masanyiwa@store.com',
+                'customer_phone' => '(+255) 778-096-234',
+                'customer_location' => 'Musoma - Mara',
+                'customer_bank' => 'NBC',
+                'customer_account' => '4567-9545-1856-9876',
+                'created_at' => date("Y-m-d H:i:s"),
+                'updated_at' => date("Y-m-d H:i:s"),
+            ],
+        ]);
+    }
+}
