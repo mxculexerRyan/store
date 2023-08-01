9d5f4443b09d8352eca5a9527b19112b7fedf657
mxculexerRyan <mxculexer@gmail.com>
2023-08-02 01:17:53 +0300

    updated assets js

--- public/frontend/assets/js/accounting/assets.js
+++ public/frontend/assets/js/accounting/assets.js
@@ -1,12 +1,16 @@
 $(document).ready(function(){
     $('.editBtn').on('click', function(){
         var id = this.id;
+        console.log(id);
         $.ajax({
             type: 'GET',
             url: '/assetdata',
             data: 'id='+id,
             success: function(data){
-                $('tbody').append(data.msg);
+                $('#editAssets_name').val(data.msg[0].asset_name);
+                $('#editAsset_amount').val(data.msg[0].asset_amount);
+                $('#editAsset_value').val(data.msg[0].asset_value);
+                $('#editAsset_type').val(data.msg[0].asset_type);
 
             }
         });
