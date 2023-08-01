3f1fc9b04d034877531af5325f9ee6f41e6349c8
mxculexerRyan <mxculexer@gmail.com>
2023-08-01 17:37:38 +0300

    updated partners blade

--- resources/views/roster/human_resource/customers.blade.php
+++ resources/views/roster/human_resource/customers.blade.php
@@ -49,7 +49,7 @@
                                 @else
                                     <td><img class="wd-30 ht-30 rounded-circle" src="{{ url('/images/30x30.PNG') }}"></td>
                                 @endif
-                                <td>{{ $item->customer_name }}</td>
+                                <td>{{ $item->name }}</td>
                                 {{-- <td>{{ $item->customer_email }}</td> --}}
                                 <td>{{ $item->customer_phone }}</td>
                                 <td>{{ $item->customer_location }}</td>
