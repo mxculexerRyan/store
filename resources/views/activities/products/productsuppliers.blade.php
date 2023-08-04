aa941b992c38000273bbaf4640669414a59615b0
mxculexerRyan <mxculexer@gmail.com>
2023-08-04 10:24:12 +0300

    update product suppliers

--- resources/views/activities/products/productsuppliers.blade.php
+++ resources/views/activities/products/productsuppliers.blade.php
@@ -1,12 +1,4 @@
-<select class="js-example-basic-single form-select form-control" id="product_supplier_{{ $id }}" name="product_name">
-    <option value="" selected disabled>Select Supplier</option>
-    @foreach ($supplierData as $key => $item)
-        <option value="{{ $item->id }}">{{ $item->name }}</option>
-    @endforeach
-</select>
-@error('product_name')
-    <span class="text-danger">{{ $message }}</span>
-@enderror
-<script>
-    initialize()
-</script>
+<option value="" selected disabled>Select Supplier</option>
+@foreach ($supplierData as $key => $item)
+    <option value="{{ $item->id }}">{{ $item->name }}</option>
+@endforeach
