768419e9985cfae4d773821b2bb173aea2aacf77
mxculexerRyan <mxculexer@gmail.com>
2023-08-04 10:23:44 +0300

    add buy controller

--- app/Http/Controllers/Trade/BuyController.php
+++ app/Http/Controllers/Trade/BuyController.php
@@ -6,6 +6,7 @@ use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Prices\Selling_price;
 use App\Models\Product;
+use App\Models\Supplier;
 use DB;
 
 class BuyController extends Controller
@@ -13,7 +14,8 @@ class BuyController extends Controller
     public function index() { 
         $productData = DB::table('buying_prices')->join('products', 'products.id', '=', 'buying_prices.product_id')->select('products.*')->distinct()->get();
         // $productData = Product::select("*")->where("product_status", "available")->get();
-        return view('activities.trade.buy', compact("productData"));
+        $supplierData = Supplier::latest()->get();
+        return view('activities.trade.buy', compact("productData", "supplierData"));
     }
 
     public function add(){
