c9997df51b2c5126f61b31fa33b5092241675e33
mxculexerRyan <mxculexer@gmail.com>
2023-08-04 08:43:43 +0300

    add product suppliers blade

--- resources/views/activities/products/productsuppliers.blade.php
+++ resources/views/activities/products/productsuppliers.blade.php
@@ -1,9 +1,12 @@
-<select class="form-select" id="product_supplier_{{ $id }}" name="product_name">
+<select class="js-example-basic-single form-select form-control" id="product_supplier_{{ $id }}" name="product_name">
     <option value="" selected disabled>Select Supplier</option>
     @foreach ($supplierData as $key => $item)
-        <option value="{{ $item->id }}">{{ $item->supplier_name }}</option>
+        <option value="{{ $item->id }}">{{ $item->name }}</option>
     @endforeach
 </select>
 @error('product_name')
     <span class="text-danger">{{ $message }}</span>
 @enderror
+<script>
+    initialize()
+</script>
