86f2b498b6f6770201fb365bd8890cfba193355a
mxculexerRyan <mxculexer@gmail.com>
2023-08-04 10:24:02 +0300

    update product list

--- resources/views/activities/products/productlist.blade.php
+++ resources/views/activities/products/productlist.blade.php
@@ -10,7 +10,17 @@
             <span class="text-danger">{{ $message }}</span>
         @enderror
     </td>
-    <td id="supplier{{ $id }}"><input type="text" class="form-control" disabled></td>
+    <td>
+        <select class="js-example-basic-single form-select form-control" id="product_supplier_{{ $id }}" name="product_name">
+        <option value="" selected disabled>Select Supplier</option>
+        @foreach ($supplierData as $key => $item)
+            <option value="{{ $item->id }}">{{ $item->name }}</option>
+        @endforeach
+    </select>
+    @error('product_name')
+        <span class="text-danger">{{ $message }}</span>
+    @enderror
+    </td>
     <td><input type="text" class="form-control" id="price{{ $id }}" name="name"></td>
     <td><input type="number" class="form-control" id="quantity{{ $id }}" name="name" disabled onkeyup="getTotal(this)" required></td>
     <td><input type="text" class="form-control" id="total{{ $id }}" name="name" disabled value="0"></td>
