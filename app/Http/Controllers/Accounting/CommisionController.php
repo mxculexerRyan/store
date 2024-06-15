fd53b7ac78758c749bdad7cf35f86956a10d217c
mxculexerRyan <mxculexer@gmail.com>
2024-06-15 08:28:35 +0300

    update commision

--- app/Http/Controllers/Accounting/CommisionController.php
+++ app/Http/Controllers/Accounting/CommisionController.php
@@ -5,12 +5,14 @@ namespace App\Http\Controllers\Accounting;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Accounting\Commision;
+use App\Models\Accounting\Account;
 
 class CommisionController extends Controller
 {
     public function index() { 
         $commisionData = Commision::latest()->get();
-        return view('roster.accounting.commisions', compact('commisionData')); 
+        $accountsData = Account::select("*")->where("account_status", "available")->get();
+        return view('roster.accounting.commisions', compact('commisionData', 'accountsData')); 
     }
 
     public function add(Request $request){
