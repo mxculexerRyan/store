184312781e62e3606e4e508662687b4d623f8ee3
mxculexerRyan <mxculexer@gmail.com>
2024-06-15 08:29:01 +0300

    update expenses

--- app/Http/Controllers/Accounting/ExpensesController.php
+++ app/Http/Controllers/Accounting/ExpensesController.php
@@ -6,12 +6,14 @@ use Illuminate\Support\Facades\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Expense;
+use App\Models\Accounting\Account;
 use DB;
 class ExpensesController extends Controller
 {
     public function index(){
         $expensesData = Expense::latest()->get();
-        return view('roster.accounting.expenses', compact('expensesData'));
+        $accountsData = Account::select("*")->where("account_status", "available")->get();
+        return view('roster.accounting.expenses', compact('expensesData', 'accountsData'));
     }
 
     public function add(Request $request){
@@ -19,16 +21,16 @@ class ExpensesController extends Controller
 
         $request->validate([
             'expense_name'          => 'required',
-            'expense_description'   => 'required',
             'expense_amount'        => 'required',
-            'paid_to'               => 'required',
+            'expense_description'   => 'required',
+            'account'               => 'required',
             'expense_receipt'       => 'required',
         ]);
 
         $expense_name           = $request->expense_name;
-        $expense_description    = $request->expense_description;
         $expense_amount         = $request->expense_amount;
-        $paid_to                = $request->paid_to;
+        $expense_description    = $request->expense_description;
+        $account_number         = $request->account;
         $expense_receipt        = "";
 
         if($request->file('expense_receipt')){
@@ -39,10 +41,10 @@ class ExpensesController extends Controller
         }
 
         $data = array(
-            'budjet_id'             => $expense_name,
-            'expense_description'   => $expense_description,
+            'expense_name'          => $expense_name,
             'expense_amount'        => $expense_amount,
-            'paid_to'               => $paid_to,
+            'expense_description'   => $expense_description ,
+            'account'               => $account_number ,
             'paid_by'               => $id,
             'expense_receipt'       => $expense_receipt,
             'created_at'            => date("Y-m-d H:i:s"),
@@ -50,8 +52,27 @@ class ExpensesController extends Controller
         );
         
         DB::table('expenses')->insert($data);
-
+        $expensesId = DB::getPdo()->lastInsertId();
+        // $payment
+        $account = Account::find($account_number);
+        $balance = $account->account_balance;
+        $account->account_balance = ($balance - $expense_amount);
+        $newbal = $account->account_balance;
+        $account->save();
         
+        $transactionsData = array(
+            'amount'    => $expense_amount,
+            'account'   => $account_number,
+            'from'      => $id ,
+            'to'        => $id ,
+            'charges'   => 0,
+            'nature'    => 'Spent',
+            'reason'    => 'Expense No: '.$expensesId,
+            'balance'   => $newbal,
+            'created_at'       => date("Y-m-d H:i:s"),
+            'updated_at'       => date("Y-m-d H:i:s"),
+        );
+        DB::table('transactions')->insert($transactionsData);
 
         $notification  = array(
         'message'    => 'New expenses Added',
--- resources/views/roster/accounting/expenses.blade.php
+++ resources/views/roster/accounting/expenses.blade.php
@@ -1,12 +1,44 @@
 <x-pagetop/>
 <div class="page-content">
 
-    <nav class="page-breadcrumb">
-        <ol class="breadcrumb">
-            <li class="breadcrumb-item"><a href="#">Tables</a></li>
-            <li class="breadcrumb-item" aria-current="page">Data Table</li>
-        </ol>
-    </nav>
+    <div class="row">
+        <div class="col-12 col-xl-12 stretch-card">
+            <div class="row flex-grow-1">
+                @foreach ($accountsData as $key => $item)
+                    <div class="col-md-4 grid-margin stretch-card">
+                        <div class="card">
+                            <div class="card-body">
+                                <div class="d-flex justify-content-between align-items-baseline">
+                                    <h6 class="mb-0 card-title">{{ $item->account_type }} Balance</h6>
+                                    <div class="mb-2 dropdown">
+                                        <a type="button" id="dropdownMenuButton" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                        <i class="icon-lg text-muted pb-3px" data-feather="more-horizontal"></i>
+                                        </a>
+                                        <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
+                                        <a class="dropdown-item d-flex align-items-center" href="javascript:;"><i data-feather="eye" class="icon-sm me-2"></i> <span class="">View</span></a>
+                                        <a class="dropdown-item d-flex align-items-center" href="javascript:;"><i data-feather="edit-2" class="icon-sm me-2"></i> <span class="">Edit</span></a>
+                                        <a class="dropdown-item d-flex align-items-center" href="javascript:;"><i data-feather="trash" class="icon-sm me-2"></i> <span class="">Delete</span></a>
+                                        <a class="dropdown-item d-flex align-items-center" href="javascript:;"><i data-feather="printer" class="icon-sm me-2"></i> <span class="">Print</span></a>
+                                        <a class="dropdown-item d-flex align-items-center" href="javascript:;"><i data-feather="download" class="icon-sm me-2"></i> <span class="">Download</span></a>
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="row">
+                                    <div class="mt-3 d-flex justify-content-between align-items-baseline">
+                                        <h2>{{ number_format($item->account_balance) }}</h2>
+                                        <p class="text-success">
+                                            <span>+3.3%</span>
+                                            <i data-feather="arrow-up" class="mb-1 icon-sm"></i>
+                                        </p>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                @endforeach
+            </div>
+        </div>
+    </div>
 
     <div class="row">
         <div class="col-md-12 grid-margin stretch-card">
@@ -29,11 +61,11 @@
                             <tr>
                                 <th>No.</th>
                                 <th>Receipt</th>
-                                <th>Budjet Name</th>
-                                <th>Description</th>
+                                <th>Expense Name</th>
                                 <th>Amount</th>
-                                <th>Receiver</th>
-                                <th>Sender</th>
+                                <th>Description</th>
+                                <th>From</th>
+                                <th>Account</th>
                                 <th>Date</th>
                                 <th>Edit</th>
                                 <th>Delete</th>
@@ -49,15 +81,14 @@
                                     <td><figure class="mb-2"><img class="rounded img-fluid" src="{{ url('/images/30x30.PNG') }}" alt="receipt"></figure></td>
                                 @endif
                                 @php
-                                    $budjet_id = $item->budjet_id; $budjetDetails = App\Models\Budjet::find($budjet_id);
-                                    $receiver_id = $item->paid_to; $receiverDetails = App\Models\Service_provider::find($receiver_id);
+                                    $accountId = $item->account; $accountDetails = App\Models\Accounting\Account::find($accountId);
                                     $payer_id = $item->paid_by; $userDetails = App\Models\User::find($payer_id);
                                 @endphp
-                                <td>{{ $budjetDetails->budjet_name }}</td>
-                                <td>{{ $item->expense_description }}</td>
+                                <td>{{ $item->expense_name }}</td>
                                 <td>{{ $item->expense_amount }}</td>
-                                <td>{{ $receiverDetails->provider_name }}</td>
+                                <td>{{ $item->expense_description }}</td>
                                 <td>{{ $userDetails->name }}</td>
+                                <td>{{ $accountDetails->account_name }} - {{ $accountDetails->account_type }}</td>
                                 <td>{{ $item->created_at }}</td>
                                 <td><button type="button" class="btn btn-inverse-warning btn-icon" data-bs-toggle="modal" data-bs-target="#editExpenseModal"><i data-feather="edit"></i></button></td>
                                 <td><button type="button" class="btn btn-inverse-danger btn-icon" onclick="showSwal('passing-parameter-execute-cancel')"><i data-feather="trash-2"></i></button></td>
