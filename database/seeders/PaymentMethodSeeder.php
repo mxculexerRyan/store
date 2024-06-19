4040b26385024437afcf21ca14fdc158eeb405c3
mxculexerRyan <mxculexer@gmail.com>
2024-06-19 15:34:32 +0300

    accounts seeders

--- database/seeders/PaymentMethodSeeder.php
+++ database/seeders/PaymentMethodSeeder.php
@@ -16,6 +16,15 @@ class PaymentMethodSeeder extends Seeder
         //
         DB::table('accounts')->insert([
             [
+                'account_type'      => 'Cash Account',
+                'account_name'      => 'Ryana Tumaini Ryana',
+                'account_number'    => '208450',
+                'account_balance'   => '0',
+                'account_status'    => 'available',
+                'created_at'        => date("Y-m-d H:i:s"),
+                'updated_at'        => date("Y-m-d H:i:s"),
+            ],
+            [
                 'account_type'      => 'NMB Bank',
                 'account_name'      => 'Ryana Tumaini Ryana',
                 'account_number'    => '302890189289291',
