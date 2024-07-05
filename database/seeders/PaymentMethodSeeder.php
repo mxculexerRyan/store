6ed38a289485cc03aca290e34cd7a96d18b650b1
mxculexerRyan <mxculexer@gmail.com>
2024-07-05 10:04:54 +0300

    pay method

--- database/seeders/PaymentMethodSeeder.php
+++ database/seeders/PaymentMethodSeeder.php
@@ -32,7 +32,16 @@ class PaymentMethodSeeder extends Seeder
                 'account_status'    => 'available',
                 'created_at'        => date("Y-m-d H:i:s"),
                 'updated_at'        => date("Y-m-d H:i:s"),
-            ]
+            ],
+            [
+                'account_type'      => 'M-pesa Lipa',
+                'account_name'      => 'Ryana Tumaini Ryana',
+                'account_number'    => '302890189289291',
+                'account_balance'   => '0',
+                'account_status'    => 'available',
+                'created_at'        => date("Y-m-d H:i:s"),
+                'updated_at'        => date("Y-m-d H:i:s"),
+            ],
         ]);
     }
 }
