64cbec4f6cb3de047fa96fac2c5588f17c49f536
mxculexerRyan <mxculexer@gmail.com>
2024-12-02 13:06:04 +0300

    order

--- app/Http/Controllers/Accounting/OrderController.php
+++ app/Http/Controllers/Accounting/OrderController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Accounting;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\Models\Order;
+use App\Models\Loans\Debtors;
 
 class OrderController extends Controller
 {
@@ -12,5 +13,25 @@ class OrderController extends Controller
         $orderData = Order::latest()->where('status', '=', 'Available')
         ->orderBy('id', 'DESC')->get();
         return view('roster.accounting.orders', compact('orderData'));
+    }
+
+    public function delete(){
+        $order_id = $_GET['id'];
+
+        $debtData = Debtors::where('reason', 'Sales of order No: '.$order_id)->first();
+        $debtData->status = "Pending";
+        $debtData->save();
+        
+        $orderData = Order::find($order_id);
+        $orderData->status = "Un-available";
+        $orderData->save(); 
+
+
+        $notification  = array(
+            'message'    => 'Order Deleted',
+            'alert-type' => 'error'
+            );
+
+        return redirect()->back()->with($notification);
     }
 }
